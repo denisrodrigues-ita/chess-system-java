16d039014ff168809927e132a1d285163164cfed
Denis Rodrigues <denisrodrigues.ita@gmail.com>
2022-02-16 22:02:22 -0300

    BoardException and defensive programming

---
+++ src/chess/ChessException.java
@@ -0,0 +1,10 @@
+package chess;
+
+public final class ChessException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public ChessException(String msg){
+        super(msg);
+    }
+}
